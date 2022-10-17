9751647c02cd0deb7fc0b781c4b2c1ad8bdc7c75
Бакланов Илья <ibaklanov@neoflex.ru>
2022-10-17 22:42:43 +0300

    fix: upd hook useFetchFriendList

--- src/shared/hooks/useFetchUserData/useFetchFriendList.js
+++ src/shared/hooks/useFetchUserData/useFetchFriendList.js
@@ -8,35 +8,45 @@ import { Observable, pipe } from 'rxjs';
 const useFetchFriendList = (accessToken) => {
 	const [friendList, setFriendList] = useState(null);
 
+	console.log('ACCESS_TOKEN: ', accessToken);
+	async function getFriendList() {
+		const list = await bridge.send('VKWebAppCallAPIMethod', {
+			method: 'friends.get',
+			request_id: '32test',
+			params: {
+				order: 'random',
+				fields: 'first_name,last_name,photo_100',
+				v: '5.131',
+				access_token: accessToken,
+			},
+		});
+
+		// console.log(list.response.count);
+		// const array = {};
+		// for (let i = 0; i < list.response.count; i++) {
+		// 	console.log(`${list.response.items[i].first_name} ${list.response.items[i].last_name}`);
+		// 	array[i] = {
+		// 		name: `${list.response.items[i].first_name} ${list.response.items[i].last_name}`,
+		// 		score: 0,
+		// 		avatarSrc: list.response.items[i].photo_100,
+		// 	};
+		// }
+
+		console.log('RESPONSE_LIST: ', list.response);
+
+		const array = list.response.items.map((item) => ({
+			name: `${item.first_name} ${item.last_name}`,
+			score: 0,
+			avatarSrc: item.photo_100,
+		}));
+		setFriendList(array);
+	}
+
 	useEffect(() => {
-		console.log(accessToken);
-		async function getFriendList() {
-			const list = await bridge.send('VKWebAppCallAPIMethod', {
-				method: 'friends.get',
-				request_id: '32test',
-				params: {
-					order: 'random',
-					fields: 'first_name,last_name,photo_100',
-					v: '5.131',
-					access_token: accessToken,
-				},
-			});
-			console.log(list.response.count);
-			const array = {};
-			for (let i = 0; i < list.response.count; i++) {
-				console.log(`${list.response.items[i].first_name} ${list.response.items[i].last_name}`);
-				array[i] = {
-					name: `${list.response.items[i].first_name} ${list.response.items[i].last_name}`,
-					score: 0,
-					avatarSrc: list.response.items[i].photo_100,
-				};
-			}
-			setFriendList(array);
-		}
 		getFriendList();
 	}, []);
 
-	return { friendList };
+	return friendList;
 };
 
 export default useFetchFriendList;
